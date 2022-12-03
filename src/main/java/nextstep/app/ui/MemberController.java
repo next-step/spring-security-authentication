36696a481ff289df8fb7ea93091eb7e6ec3e08e8
Shinye Song <nicole.luvsong@kakaostyle.com>
2022-12-03 14:51:55 +0900

    feat: Add get members api with http basic authorization

--- src/main/java/nextstep/app/ui/MemberController.java
+++ src/main/java/nextstep/app/ui/MemberController.java
@@ -1,9 +1,13 @@
 package nextstep.app.ui;
 
 import nextstep.app.domain.Member;
-import nextstep.app.domain.MemberRepository;
+import nextstep.app.domain.MemberService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,16 +15,38 @@ import java.util.List;
 @RestController
 public class MemberController {
 
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
-    public MemberController(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
     }
 
     @GetMapping("/members")
-    public ResponseEntity<List<Member>> list() {
-        List<Member> members = memberRepository.findAll();
+    public ResponseEntity<List<Member>> list(@RequestHeader String authorization) {
+
+        String[] split = authorization.split(" ");
+        String type = split[0];
+        String credential = split[1];
+
+        if (!"Basic".equalsIgnoreCase(type)) {
+            throw new AuthenticationException();
+        }
+
+        String decodedCredential = new String(Base64Utils.decodeFromString(credential));
+        String[] emailAndPassword = decodedCredential.split(":");
+
+        String email = emailAndPassword[0];
+        String password = emailAndPassword[1];
+
+        memberService.isValidMemberInfo(email, password);
+
+        List<Member> members = memberService.getMembers();
+
         return ResponseEntity.ok(members);
     }
 
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<Void> handleAuthenticationException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
 }
