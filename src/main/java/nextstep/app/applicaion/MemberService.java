0bdbe5f021979767b930bd3af946017f17932b5e
hyunssooo <jhs695@gmail.com>
2022-12-04 13:06:13 +0900

    feat: UserDetailsService 추가

---
+++ src/main/java/nextstep/app/applicaion/MemberService.java
@@ -0,0 +1,25 @@
+package nextstep.app.applicaion;
+
+import nextstep.app.domain.Member;
+import nextstep.app.domain.MemberRepository;
+import nextstep.security.exception.AuthenticationException;
+import nextstep.security.userdetils.User;
+import nextstep.security.userdetils.UserDetails;
+import nextstep.security.userdetils.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        final Member member = memberRepository.findByEmail(username)
+            .orElseThrow(AuthenticationException::new);
+        return new User(member.getEmail(), member.getPassword());
+    }
+}
