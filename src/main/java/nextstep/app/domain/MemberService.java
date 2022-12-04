15dd2ac63bdfb93a7dafc5e9308a5516529e9f9e
jaylin.re <an.jin@croquis.com>
2022-12-04 19:42:56 +0900

    refactor: Member authentication 체크용 클래스 분리

--- src/main/java/nextstep/app/domain/MemberService.java
+++ src/main/java/nextstep/app/domain/MemberService.java
@@ -1,13 +1,11 @@
 package nextstep.app.domain;
 
-import nextstep.security.authentication.UserAuthenticationService;
-import nextstep.security.exception.AuthenticationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class MemberService implements UserAuthenticationService {
+public class MemberService {
     private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
@@ -18,11 +16,4 @@ public class MemberService implements UserAuthenticationService {
         return memberRepository.findAll();
     }
 
-    public void validateMember(String email, String password) {
-        Member member = memberRepository.findByEmail(email).orElseThrow(AuthenticationException::new);
-
-        if (!member.getPassword().equals(password)) {
-            throw new AuthenticationException();
-        }
-    }
 }
