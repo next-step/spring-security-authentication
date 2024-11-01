763f9508e5eb480ec2a385a295fb401f9f38a0e1
Jealyang <jaelyangchoi@gmail.com>
2024-11-01 22:13:19 +0900

    로그인 구현

--- src/main/java/nextstep/app/ui/LoginController.java
+++ src/main/java/nextstep/app/ui/LoginController.java
@@ -1,5 +1,7 @@
 package nextstep.app.ui;
 
+import lombok.RequiredArgsConstructor;
+import nextstep.app.domain.Member;
 import nextstep.app.domain.MemberRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,19 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class LoginController {
     public static final String SPRING_SECURITY_CONTEXT_KEY = "SPRING_SECURITY_CONTEXT";
 
     private final MemberRepository memberRepository;
 
-    public LoginController(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
     @PostMapping("/login")
     public ResponseEntity<Void> login(HttpServletRequest request, HttpSession session) {
+        String email = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        Member member = memberRepository.findByEmail(email).orElseThrow(AuthenticationException::new);
+        if (email.equals(member.getEmail()) && password.equals(member.getPassword())) {
+            session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, member); //session name, object
+        } else {
+            throw new AuthenticationException();
+        }
+        
         return ResponseEntity.ok().build();
     }
 
