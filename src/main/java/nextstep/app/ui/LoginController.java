1d3284ce3dac3779590aa23a7107b0bfb2a6c00a
younsikkim <blackeyeys@naver.com>
2024-11-02 12:14:46 +0900

    refactor : Optimize code

--- src/main/java/nextstep/app/ui/LoginController.java
+++ src/main/java/nextstep/app/ui/LoginController.java
@@ -1,27 +1,15 @@
 package nextstep.app.ui;
 
 import lombok.RequiredArgsConstructor;
-import nextstep.app.constants.AppConstants;
-import nextstep.app.domain.Member;
-import nextstep.app.domain.MemberRepository;
-import nextstep.app.service.MemberService;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import static nextstep.app.constants.AppConstants.SPRING_SECURITY_CONTEXT_KEY;
-
 @RequiredArgsConstructor
 @RestController
 public class LoginController {
-    private final MemberService memberService;
-
     @PostMapping("/login")
     public ResponseEntity<Void> login() {
         return ResponseEntity.ok().build();
