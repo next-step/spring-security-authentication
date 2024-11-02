fc19cae06902ef0882d94fd37d5af317c21b7aa5
박원종/웹서비스서버개발Cell/Kia <wonjong@kia.com>
2024-11-02 11:52:42 +0900

    feat: Basic 유저 정보 추출

--- src/main/java/nextstep/app/ui/MemberController.java
+++ src/main/java/nextstep/app/ui/MemberController.java
@@ -3,9 +3,12 @@ package nextstep.app.ui;
 import nextstep.app.domain.Member;
 import nextstep.app.domain.MemberRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
@@ -18,9 +21,24 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    public ResponseEntity<List<Member>> list() {
+    public ResponseEntity<List<Member>> list(HttpServletRequest request, HttpSession session) {
         List<Member> members = memberRepository.findAll();
+
+        String basicToken = request.getHeader("Authorization");
+
+        authenticate(basicToken);
+
         return ResponseEntity.ok(members);
     }
 
+    private void authenticate(String token) {
+        String payload = token.split(" ")[1];
+
+        String decodedPayload = new String(Base64Utils.decodeFromString(payload), StandardCharsets.UTF_8);
+        String[] memberInfo = decodedPayload.split(":");
+
+        if (memberInfo.length != 2) {
+            throw new AuthenticationException();
+        }
+    }
 }
