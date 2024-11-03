60b7df168dc68100cdf125863be9f70f915beac7
Jealyang <jaelyangchoi@gmail.com>
2024-11-03 18:22:18 +0900

    polishing

--- src/main/java/nextstep/security/authorization/AccessDeniedException.java
+++ src/main/java/nextstep/security/authorization/AccessDeniedException.java
@@ -5,8 +5,8 @@ package nextstep.security.authorization;
  */
 public class AccessDeniedException extends RuntimeException {
 
-    public AccessDeniedException(String msg) {super(msg);}
-    public AccessDeniedException(String msg, Throwable cause) {
-        super(msg, cause);
+    public AccessDeniedException(String msg) {
+        super(msg);
     }
+
 }
