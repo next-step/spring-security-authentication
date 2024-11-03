7a887357bbdc9f7ab125ae67105fe49b2b3a7610
이준현 <dlwnsgus07@kia.com>
2024-11-03 14:59:05 +0900

    feat : BasicAuthInterceptor 구현
    Authorization Header의 Basic 토큰을 가져와 사용자를 식별하는 BasicAuthInterceptor 구현

--- src/main/java/nextstep/app/config/WebConfig.java
+++ src/main/java/nextstep/app/config/WebConfig.java
@@ -1,6 +1,8 @@
 package nextstep.app.config;
 
+import nextstep.security.authentication.BasicAuthInterceptor;
 import nextstep.security.authentication.FormLoginAuthInterceptor;
+import nextstep.security.authentication.TokenDecoder;
 import nextstep.security.userdetail.UserDetailService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,14 +12,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final UserDetailService userDetailService;
+    private final TokenDecoder tokenDecoder;
 
-    public WebConfig(UserDetailService userDetailService) {
+    public WebConfig(UserDetailService userDetailService, TokenDecoder tokenDecoder) {
         this.userDetailService = userDetailService;
+        this.tokenDecoder = tokenDecoder;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new FormLoginAuthInterceptor(userDetailService))
                 .addPathPatterns("/login");
+        registry.addInterceptor(new BasicAuthInterceptor(tokenDecoder, userDetailService))
+                .addPathPatterns("/members");
     }
 }
---
+++ src/main/java/nextstep/security/authentication/BasicAuthInterceptor.java
@@ -0,0 +1,41 @@
+package nextstep.security.authentication;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import nextstep.security.userdetail.UserDetail;
+import nextstep.security.userdetail.UserDetailService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class BasicAuthInterceptor implements HandlerInterceptor {
+
+    public static final String SPRING_SECURITY_CONTEXT_KEY = "SPRING_SECURITY_CONTEXT";
+
+    private final TokenDecoder tokenDecoder;
+    private final UserDetailService userDetailService;
+
+
+    public BasicAuthInterceptor(TokenDecoder tokenDecoder, UserDetailService userDetailService) {
+        this.tokenDecoder = tokenDecoder;
+        this.userDetailService = userDetailService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+            Object handler) throws Exception {
+        try {
+            String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+            UserDetail decodedUserInfo = tokenDecoder.decodeToken(token);
+
+            UserDetail userDetail = userDetailService.getUserDetail(decodedUserInfo.getUsername(),
+                    decodedUserInfo.getPassword());
+
+            request.getSession().setAttribute(SPRING_SECURITY_CONTEXT_KEY, userDetail);
+            return true;
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+    }
+}
