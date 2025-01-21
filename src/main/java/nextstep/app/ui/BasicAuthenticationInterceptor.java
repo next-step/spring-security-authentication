4607bd08fbfc3d77cc8f245e6a7298dcec8cef12
boorownie <probitanima11@gmail.com>
2025-01-21 20:03:33 +0900

    practice - interceptor

---
+++ src/main/java/nextstep/app/ui/BasicAuthenticationInterceptor.java
@@ -0,0 +1,36 @@
+package nextstep.app.ui;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.app.domain.MemberRepository;
+import nextstep.app.util.Base64Convertor;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class BasicAuthenticationInterceptor implements HandlerInterceptor {
+    private final MemberRepository memberRepository;
+
+    public BasicAuthenticationInterceptor(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        try {
+            String authorization = request.getHeader("Authorization");
+            String credentials = authorization.split(" ")[1];
+            String decodedString = Base64Convertor.decode(credentials);
+            String[] usernameAndPassword = decodedString.split(":");
+            String username = usernameAndPassword[0];
+            String password = usernameAndPassword[1];
+
+            memberRepository.findByEmail(username)
+                    .filter(it -> it.matchPassword(password))
+                    .orElseThrow(AuthenticationException::new);
+
+            return true;
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+    }
+}
