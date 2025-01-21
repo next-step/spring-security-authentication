b94cbbe10309d47571c04389ce1376021e0b588b
boorownie <probitanima11@gmail.com>
2025-01-21 20:03:33 +0900

    practice - interceptor

---
+++ src/main/java/nextstep/app/WebConfig.java
@@ -0,0 +1,22 @@
+package nextstep.app;
+
+import nextstep.app.domain.MemberRepository;
+import nextstep.app.ui.BasicAuthenticationInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final MemberRepository memberRepository;
+
+    public WebConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new FormLoginInterceptor(memberRepository)).addPathPatterns("/login");
+        registry.addInterceptor(new BasicAuthenticationInterceptor(memberRepository)).addPathPatterns("/members");
+    }
+}
