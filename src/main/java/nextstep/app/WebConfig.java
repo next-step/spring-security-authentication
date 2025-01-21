90e058216bbe33d7a88f5cdeef81b2f5b25f1d39
boorownie <probitanima11@gmail.com>
2025-01-21 20:12:06 +0900

    practice - user details refactoring

--- src/main/java/nextstep/app/WebConfig.java
+++ src/main/java/nextstep/app/WebConfig.java
@@ -1,8 +1,12 @@
 package nextstep.app;
 
+import nextstep.app.domain.Member;
 import nextstep.app.domain.MemberRepository;
 import nextstep.app.ui.BasicAuthenticationInterceptor;
 import nextstep.app.ui.FormLoginInterceptor;
+import nextstep.security.UserDetails;
+import nextstep.security.UserDetailsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,7 +21,26 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new FormLoginInterceptor(memberRepository)).addPathPatterns("/login");
-        registry.addInterceptor(new BasicAuthenticationInterceptor(memberRepository)).addPathPatterns("/members");
+        registry.addInterceptor(new FormLoginInterceptor(userDetailsService())).addPathPatterns("/login");
+        registry.addInterceptor(new BasicAuthenticationInterceptor(userDetailsService())).addPathPatterns("/members");
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            Member member = memberRepository.findByEmail(username)
+                    .orElseThrow(() -> new IllegalArgumentException("해당하는 사용자를 찾을 수 없습니다."));
+            return new UserDetails() {
+                @Override
+                public String getUsername() {
+                    return member.getEmail();
+                }
+
+                @Override
+                public String getPassword() {
+                    return member.getPassword();
+                }
+            };
+        };
     }
 }
