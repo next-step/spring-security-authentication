c4a2d641ca66b9f86806defe69411c62d1f14f30
beng9re <kbh0581@gmail.com>
2025-02-04 23:52:29 +0900

    feat(step2): 제공된 AuthenticationProvider를 기반으로 DaoAuthenticationProvider 구현

---
+++ src/test/java/nextstep/security/provider/DaoAuthenticationProviderTest.java
@@ -0,0 +1,81 @@
+package nextstep.security.provider;
+
+
+import nextstep.security.Authentication;
+import nextstep.security.AuthenticationException;
+import nextstep.security.UsernamePasswordAuthenticationToken;
+import nextstep.security.fixture.TestUserDetails;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+class DaoAuthenticationProviderTest {
+    @Test
+    @DisplayName("daoAuthenticationProvider 는 UsernamePasswordAuthenticationToken 만 지원한다")
+    void support() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider((it) -> null);
+
+        assertSoftly(it -> {
+            it.assertThat(daoAuthenticationProvider.supports(UsernamePasswordAuthenticationToken.class)).isTrue();
+            it.assertThat(daoAuthenticationProvider.supports(Authentication.class)).isFalse();
+        });
+    }
+
+    @Test
+    @DisplayName("authentication 가 비어있으면 예외를 발생한다")
+    void noAuthentication() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider((it) -> null);
+
+        assertThatExceptionOfType(AuthenticationException.class).isThrownBy(
+                () -> daoAuthenticationProvider.authenticate(null)
+        );
+    }
+
+    @Test
+    @DisplayName("principal 이 비어있으면 예외가 발생한다")
+    void noPrincipal() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider((it) -> null);
+
+        assertThatExceptionOfType(AuthenticationException.class).isThrownBy(
+                () -> daoAuthenticationProvider.authenticate(new UsernamePasswordAuthenticationToken(null, ""))
+        );
+    }
+
+    @Test
+    @DisplayName("credentials이 비어 있으면 예외가 발생한다")
+    void noCredentials() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider((it) -> null);
+
+        assertThatExceptionOfType(AuthenticationException.class).isThrownBy(
+                () -> daoAuthenticationProvider.authenticate(new UsernamePasswordAuthenticationToken("", null))
+        );
+    }
+
+    @Test
+    @DisplayName("자격증명이 다른경우 예외를 발생한다")
+    void notValidCredentials() {
+        String password = "password";
+        TestUserDetails userDetail = new TestUserDetails("id", password);
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider((it) -> userDetail);
+
+        assertThatExceptionOfType(AuthenticationException.class).isThrownBy(
+                () -> daoAuthenticationProvider.authenticate(new UsernamePasswordAuthenticationToken("id", "noPassword"))
+        );
+    }
+
+    @Test
+    @DisplayName("자격증명이 올바른경우 인증된 authentication 가 반환된다")
+    void authenticate() {
+        String password = "password";
+        TestUserDetails userDetail = new TestUserDetails("id", password);
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider((it) -> userDetail);
+
+        Authentication authenticate = daoAuthenticationProvider.authenticate(new UsernamePasswordAuthenticationToken("", password));
+
+        assertThat(authenticate.isAuthenticated()).isTrue();
+    }
+}
