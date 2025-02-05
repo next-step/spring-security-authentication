bbce6a90d9440aa3b611103539facdf352b9c378
pilming44 <pilming44@naver.com>
2025-02-05 21:50:27 +0900

    refactor: BasicAuthenticationFilter에 MATCH_URI 추가

--- src/main/java/nextstep/security/filter/BasicAuthenticationFilter.java
+++ src/main/java/nextstep/security/filter/BasicAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package nextstep.security.filter;
 
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.security.exception.AuthenticationException;
@@ -9,15 +10,22 @@ import nextstep.security.user.UserDetailsService;
 import nextstep.security.util.Base64Convertor;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
 public class BasicAuthenticationFilter extends OncePerRequestFilter {
     private final UserDetailsService userDetailsService;
+    private static final String MATCH_URI = "/members";
 
     public BasicAuthenticationFilter(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (!request.getRequestURI().equals(MATCH_URI)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
         try {
             String authorization = request.getHeader("Authorization");
             String credentials = authorization.split(" ")[1];
