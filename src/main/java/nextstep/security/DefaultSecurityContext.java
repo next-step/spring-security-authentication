2f9a6799b4d051fd6bfb538085289fb196bb060f
beng9re <kbh0581@gmail.com>
2025-02-05 21:08:03 +0900

    feat(step3): SecurityContext 및 SecurityContextHolder 작성

---
+++ src/main/java/nextstep/security/DefaultSecurityContext.java
@@ -0,0 +1,15 @@
+package nextstep.security;
+
+public class DefaultSecurityContext implements SecurityContext {
+    private Authentication authentication;
+
+    @Override
+    public Authentication getAuthentication() {
+        return authentication;
+    }
+
+    @Override
+    public void setAuthentication(Authentication authentication) {
+        this.authentication = authentication;
+    }
+}
