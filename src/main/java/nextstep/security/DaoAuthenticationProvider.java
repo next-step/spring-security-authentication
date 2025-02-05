b419d2aa78f4203cdcdb8b1dc4f8c9adc937bc86
KimJuHyun <rlawngus0910@naver.com>
2025-02-05 20:44:08 +0900

    feat: AuthenticationProvider, DaoAuthenticationProvider 구현

---
+++ src/main/java/nextstep/security/DaoAuthenticationProvider.java
@@ -0,0 +1,31 @@
+package nextstep.security;
+
+import nextstep.security.domain.MemberDetail;
+import nextstep.security.domain.MemberDetailService;
+import nextstep.security.exception.AuthenticationException;
+
+public class DaoAuthenticationProvider implements AuthenticationProvider {
+
+    private final MemberDetailService memberDetailService;
+
+    public DaoAuthenticationProvider(MemberDetailService memberDetailService) {
+        this.memberDetailService = memberDetailService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        MemberDetail member = memberDetailService.findByUsername((String) authentication.getPrincipal());
+        if (!member.isCorrectPassword((String) authentication.getCredentials())) {
+            throw new AuthenticationException();
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(member, null);
+        authenticationToken.setAuthenticated(true);
+        return authenticationToken;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.isAssignableFrom(UsernamePasswordAuthenticationToken.class);
+    }
+}
