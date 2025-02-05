dbaefecd7b4e19b744d1a4f39d605d13dfc571f9
KimJuHyun <rlawngus0910@naver.com>
2025-02-05 20:42:49 +0900

    feat: AuthenticationManager, ProviderManager 구현

---
+++ src/main/java/nextstep/security/ProviderManager.java
@@ -0,0 +1,23 @@
+package nextstep.security;
+
+import java.util.List;
+
+public class ProviderManager implements AuthenticationManager {
+
+    List<AuthenticationProvider> providers;
+
+    public ProviderManager(List<AuthenticationProvider> providers) {
+        this.providers = providers;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+        for (AuthenticationProvider provider : providers) {
+            if (provider.supports(authentication.getClass())) {
+                return provider.authenticate(authentication);
+            }
+        }
+
+        return null;
+    }
+}
