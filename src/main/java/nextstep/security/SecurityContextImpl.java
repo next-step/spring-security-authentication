6afd4976ed1d23fac6c768ccc2165beaa14e5995
KimJuHyun <rlawngus0910@naver.com>
2025-02-05 21:43:58 +0900

    feat: SecurityContext, SecurityContextHolder 추가

---
+++ src/main/java/nextstep/security/SecurityContextImpl.java
@@ -0,0 +1,15 @@
+package nextstep.security;
+
+public class SecurityContextImpl implements SecurityContext {
+    private Authentication authentication;
+
+    @Override
+    public Authentication getAuthentication() {
+        return authentication;
+    }
+
+    @Override
+    public void setAuthentication(Authentication authentication) {
+        this.authentication = authentication;
+    }
+}
