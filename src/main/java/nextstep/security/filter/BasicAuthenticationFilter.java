432bd2e3cd3506be55e6598a130fea2ed26e3c8c
KimJuHyun <rlawngus0910@naver.com>
2025-02-06 21:28:23 +0900

    refactor: BasicAuthenticationFilter 리팩토링
    - Username, Password 파싱 로직 메서드 추출
    - 필터 타겟 확인 로직 메서드 추출

--- src/main/java/nextstep/security/filter/BasicAuthenticationFilter.java
+++ src/main/java/nextstep/security/filter/BasicAuthenticationFilter.java
@@ -2,15 +2,14 @@ package nextstep.security.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-import nextstep.security.Authentication;
-import nextstep.security.AuthenticationManager;
-import nextstep.security.SecurityContextHolder;
-import nextstep.security.UsernamePasswordAuthenticationToken;
+import nextstep.security.*;
 import nextstep.security.exception.AuthenticationException;
 import nextstep.security.util.Base64Convertor;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
+
+import static nextstep.security.filter.LoginAuthenticationFilter.SPRING_SECURITY_CONTEXT_KEY;
 
 public class BasicAuthenticationFilter extends GenericFilterBean {
 
@@ -22,34 +21,60 @@ public class BasicAuthenticationFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-
-        if (!httpRequest.getRequestURI().startsWith("/members")) {
+        if (notTarget(httpRequest)) {
             chain.doFilter(request, response);
             return;
         }
 
         try {
-            String authorization = httpRequest.getHeader("Authorization");
-            String credentials = authorization.split(" ")[1];
-            String decodedString = Base64Convertor.decode(credentials);
-            String[] usernameAndPassword = decodedString.split(":");
-            String username = usernameAndPassword[0];
-            String password = usernameAndPassword[1];
-
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, password);
-            Authentication resultAuthentication = authenticationManager.authenticate(authentication);
+            Authentication resultAuthentication = authenticationManager.authenticate(getAuthenticationFrom(httpRequest));
             SecurityContextHolder.getContext().setAuthentication(resultAuthentication);
-        } catch (Exception e) {
+        } catch (AuthenticationException e) {
             SecurityContextHolder.clearContext();
-            throw new AuthenticationException();
+            throw e;
         }
 
         try {
             chain.doFilter(request, response);
         } finally {
             SecurityContextHolder.clearContext();
+        }
+    }
+
+    private static boolean notTarget(HttpServletRequest httpRequest) {
+        return !httpRequest.getRequestURI().startsWith("/members");
+    }
+
+    private static Authentication getAuthenticationFrom(HttpServletRequest httpRequest) {
+        if (httpRequest.getSession().getAttribute(SPRING_SECURITY_CONTEXT_KEY) != null) {
+            SecurityContext attribute = (SecurityContext) httpRequest.getSession().getAttribute(SPRING_SECURITY_CONTEXT_KEY);
+            return attribute.getAuthentication();
+        }
+
+        String basicToken = extractToken(httpRequest);
+        String[] usernameAndPassword = splitToken(basicToken);
+
+        String username = usernameAndPassword[0];
+        String password = usernameAndPassword[1];
+        return new UsernamePasswordAuthenticationToken(username, password);
+    }
+
+    private static String extractToken(HttpServletRequest httpRequest) {
+        String authorization = httpRequest.getHeader("Authorization");
+        return authorization.split(" ")[1];
+    }
+
+    private static String[] splitToken(String basicToken) {
+        String decodedString = Base64Convertor.decode(basicToken);
+        String[] usernameAndPassword = decodedString.split(":");
+        validateToken(usernameAndPassword);
+        return usernameAndPassword;
+    }
+
+    private static void validateToken(String[] usernameAndPassword) {
+        if (usernameAndPassword.length != 2) {
+            throw new AuthenticationException();
         }
     }
 }
