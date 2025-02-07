9c4473b03e36224dae519974e5bf0dbe6a878e77
pilming44 <pilming44@naver.com>
2025-02-07 12:17:35 +0900

    fix: BasicAuthenticationFilter 에서 인증헤더 "basic " 검사추가

--- src/main/java/nextstep/security/filter/BasicAuthenticationFilter.java
+++ src/main/java/nextstep/security/filter/BasicAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import nextstep.security.authentication.*;
 import nextstep.security.user.UserDetailsService;
 import nextstep.security.util.Base64Convertor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -22,12 +23,11 @@ public class BasicAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
-            String authorization = request.getHeader("Authorization");
-            if (authorization == null) {
+            String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+            if (authorization == null || !authorization.startsWith("Basic ")) {
                 filterChain.doFilter(request, response);
                 return;
             }
-
             String credentials = authorization.split(" ")[1];
             String decodedString = Base64Convertor.decode(credentials);
             String[] usernameAndPassword = decodedString.split(":");
