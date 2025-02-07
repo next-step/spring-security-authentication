1b974e2e305f61645f6418bb75f41a8682d7d36f
juno-junho <ssmm0205@naver.com>
2025-02-08 01:30:57 +0900

    feat(Authentication): FilterChainProxy 구현

--- src/main/java/nextstep/security/config/FilterChainProxy.java
+++ src/main/java/nextstep/security/config/FilterChainProxy.java
@@ -5,6 +5,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
@@ -12,39 +14,70 @@ import java.util.List;
 
 public class FilterChainProxy extends GenericFilterBean {
 
-    private final List<SecurityFilterChain> securityFilterChains;
+    private final List<SecurityFilterChain> filterChains;
 
-    public FilterChainProxy(List<SecurityFilterChain> securityFilterChains) {
-        this.securityFilterChains = securityFilterChains;
+    private VirtualFilterChainDecorator virtualFilterChainDecorator = new VirtualFilterChainDecorator();
+
+    public FilterChainProxy(List<SecurityFilterChain> filterChains) {
+        this.filterChains = filterChains;
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        List<Filter> filters = getFilters(request);
+        
+        if (filters == null || filters.isEmpty()) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        this.virtualFilterChainDecorator.decorate(filterChain, filters).doFilter(request, response);
+    }
+
+    private List<Filter> getFilters(HttpServletRequest request) {
+        return this.filterChains.stream()
+                .filter(chain -> chain.matches(request))
+                .findAny()
+                .map(SecurityFilterChain::getFilters)
+                .orElse(null);
+    }
+
+    public static final class VirtualFilterChainDecorator {
+
+        public FilterChain decorate(FilterChain original, List<Filter> filters) {
+            return new VirtualFilterChain(original, filters);
+        }
 
     }
 
     private static class VirtualFilterChain implements FilterChain {
 
-        private final List<Filter> filters;
         private final FilterChain originalChain;
+        private final List<Filter> additionalFilters;
+        private final int size;
+
         private int currentPosition = 0;
 
-        public VirtualFilterChain(List<Filter> filters, FilterChain originalChain) {
-            this.filters = filters;
+        public VirtualFilterChain(FilterChain originalChain, List<Filter> additionalFilters) {
             this.originalChain = originalChain;
+            this.additionalFilters = additionalFilters;
+            this.size = additionalFilters.size();
         }
 
         @Override
         public void doFilter(ServletRequest request, ServletResponse response) throws IOException, ServletException {
-            if (currentPosition == filters.size()) {
-                originalChain.doFilter(request, response);
-            } else {
-                Filter nextFilter = filters.get(currentPosition++);
-                nextFilter.doFilter(request, response, this);
+            if (this.currentPosition == this.size) {
+                this.originalChain.doFilter(request, response);
+                return;
             }
+            this.currentPosition++;
+            Filter nextFilter = this.additionalFilters.get(this.currentPosition - 1);
+
+            nextFilter.doFilter(request, response, this);
         }
 
     }
-
 
 }
