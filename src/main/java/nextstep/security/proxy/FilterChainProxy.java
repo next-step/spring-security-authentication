6c8af9bf264059acdd3f6b522941077d03ea1e12
pilming44 <pilming44@naver.com>
2025-02-07 12:08:18 +0900

    style: 오타수정 fiter -> filter

--- src/main/java/nextstep/security/proxy/FilterChainProxy.java
+++ src/main/java/nextstep/security/proxy/FilterChainProxy.java
@@ -8,16 +8,16 @@ import java.io.IOException;
 import java.util.List;
 
 public class FilterChainProxy extends GenericFilterBean {
-    private final List<SecurityFilterChain> fiterChains;
+    private final List<SecurityFilterChain> filterChains;
 
-    public FilterChainProxy(List<SecurityFilterChain> fiters) {
-        this.fiterChains = fiters;
+    public FilterChainProxy(List<SecurityFilterChain> filters) {
+        this.filterChains = filters;
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain originalChain) throws IOException, ServletException {
 
-        List<SecurityFilterChain> supportedChain = fiterChains.stream()
+        List<SecurityFilterChain> supportedChain = filterChains.stream()
                 .filter(chain -> chain.supports(servletRequest))
                 .toList();
 
@@ -29,21 +29,21 @@ public class FilterChainProxy extends GenericFilterBean {
     }
 
     public class VirtualFilterChain implements FilterChain {
-        private final List<Filter> fiters;
+        private final List<Filter> filters;
         private final FilterChain originalChain;
         private int currentPosition = 0;
 
-        public VirtualFilterChain(List<Filter> fiters, FilterChain originalChain) {
-            this.fiters = fiters;
+        public VirtualFilterChain(List<Filter> filters, FilterChain originalChain) {
+            this.filters = filters;
             this.originalChain = originalChain;
         }
 
         @Override
         public void doFilter(ServletRequest request, ServletResponse response) throws IOException, ServletException {
-            if (currentPosition == fiters.size()) {
+            if (currentPosition == filters.size()) {
                 originalChain.doFilter(request, response);
             } else {
-                Filter filter = fiters.get(currentPosition++);
+                Filter filter = filters.get(currentPosition++);
                 filter.doFilter(request, response, this);
             }
         }
