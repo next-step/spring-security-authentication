1426d35cdd0493311372270a9c7f39d67d006812
pilming44 <pilming44@naver.com>
2025-02-08 19:04:53 +0900

    refactor: 기존 세션이 있는 경우에만 SecurityContext를 로드하도록 수정

--- src/main/java/nextstep/security/authentication/HttpSessionSecurityContextRepository.java
+++ src/main/java/nextstep/security/authentication/HttpSessionSecurityContextRepository.java
@@ -4,13 +4,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+import java.util.Objects;
+
 public class HttpSessionSecurityContextRepository implements SecurityContextRepository {
     public static final String SPRING_SECURITY_CONTEXT_KEY = "SPRING_SECURITY_CONTEXT";
 
     @Override
     public SecurityContext loadContext(HttpServletRequest request) {
-        HttpSession session = request.getSession();
-
+        HttpSession session = request.getSession(false);
+        if (Objects.isNull(session)) {
+            return null;
+        }
         return (SecurityContext) session.getAttribute(SPRING_SECURITY_CONTEXT_KEY);
     }
 
