83dfdebae84354dcae2f19ff3084e06fd3b564c5
evan <evan@danal.co.kr>
2025-02-08 15:57:00 +0900

    feat: spring configuration 에서 받은 delegationFilterProxy 추가

---
+++ src/main/java/nextstep/security/web/DelegatingFilterProxy.java
@@ -0,0 +1,23 @@
+package nextstep.security.web;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class DelegatingFilterProxy extends GenericFilterBean {
+
+    private final FilterChainProxy filterChainProxy;
+
+    public DelegatingFilterProxy(FilterChainProxy filterChainProxy) {
+        this.filterChainProxy = filterChainProxy;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChainProxy.doFilter(servletRequest, servletResponse, filterChain);
+    }
+}
