b782aa906a221e2a710a32bc70ea044b975a302e
evan <evan@danal.co.kr>
2025-02-08 15:57:23 +0900

    feat: FormLoginAuthenticationFilter 추가

---
+++ src/main/java/nextstep/security/filter/FormLoginAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package nextstep.security.filter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.security.authentication.Authentication;
+import nextstep.security.authentication.context.SecurityContextImpl;
+import nextstep.security.authentication.context.SecurityContextRepository;
+import nextstep.security.authentication.context.SecurityContextRepositoryImpl;
+import nextstep.security.authentication.provider.AuthenticationManager;
+import nextstep.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.io.IOException;
+
+public class FormLoginAuthenticationFilter extends AbstractAuthenticationFilter {
+
+    private static final String SPRING_SECURITY_FORM_USERNAME = "username";
+    private static final String SPRING_SECURITY_FORM_PASSWORD = "password";
+
+    private final SecurityContextRepository securityContextRepository;
+
+    public FormLoginAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+        this.securityContextRepository = SecurityContextRepositoryImpl.getInstance();
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        if(!support(request)) {
+            return null;
+        }
+
+        final String username = request.getParameter(SPRING_SECURITY_FORM_USERNAME);
+        final String password = request.getParameter(SPRING_SECURITY_FORM_PASSWORD);
+
+        Authentication authentication = this.authenticationManager.authentication(
+                new UsernamePasswordAuthenticationToken(username, password));
+
+        SecurityContextImpl securityContext = SecurityContextImpl.from(authentication);
+
+        securityContextRepository.saveContext(securityContext, request);
+
+        return authentication;
+    }
+
+    @Override
+    public boolean support(HttpServletRequest request) {
+        return request.getMethod().equals("POST") &&
+                request.getPathInfo().equals("/login");
+    }
+}
