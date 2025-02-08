7e2622661bd74f78bf1ee02d56c080f2b29ed57c
evan <evan@danal.co.kr>
2025-02-08 15:59:03 +0900

    feat: securityContext Holder 추가

---
+++ src/main/java/nextstep/security/authentication/context/SecurityContextHolder.java
@@ -0,0 +1,24 @@
+package nextstep.security.authentication.context;
+
+public class SecurityContextHolder {
+    private static final ThreadLocal<SecurityContext> contextHolder = new ThreadLocal<>();
+
+    public static SecurityContext getContext() {
+        SecurityContext context = contextHolder.get();
+        if (context == null) {
+            context = new SecurityContextImpl();
+            contextHolder.set(context);
+        }
+        return context;
+    }
+
+    public static void setContext(SecurityContext context) {
+        contextHolder.set(context);
+    }
+
+    public static void clearContext() {
+        contextHolder.remove();
+    }
+
+}
+
---
+++ src/main/java/nextstep/security/filter/SecurityContextHolderFilter.java
@@ -0,0 +1,32 @@
+package nextstep.security.filter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.security.authentication.Authentication;
+import nextstep.security.authentication.context.SecurityContext;
+import nextstep.security.authentication.context.SecurityContextHolder;
+import nextstep.security.authentication.context.SecurityContextRepository;
+import nextstep.security.authentication.context.SecurityContextRepositoryImpl;
+import nextstep.security.authentication.provider.AuthenticationManager;
+
+public class SecurityContextHolderFilter extends AbstractAuthenticationFilter{
+
+    private final SecurityContextRepository securityContextRepository;
+
+    public SecurityContextHolderFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+        this.securityContextRepository = SecurityContextRepositoryImpl.getInstance();
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContext securityContext = securityContextRepository.loadContext(request);
+        SecurityContextHolder.setContext(securityContext);
+        return securityContext.getAuthentication();
+    }
+
+    @Override
+    public boolean support(HttpServletRequest request) {
+        return true;
+    }
+}
