b60509a91866da19f571aa03a2d2639d4d5b8e73
shinseongsu <evan@danal.co.kr>
2025-02-09 09:26:07 +0900

    refactor: 사용하지 않는 클래스 삭제

--- src/main/java/nextstep/security/exception/AuthenticationServletException.java
+++
@@ -1,7 +0,0 @@
-package nextstep.security.exception;
-
-public class AuthenticationServletException extends RuntimeException {
-    public AuthenticationServletException(String message) {
-        super(message);
-    }
-}
--- src/main/java/nextstep/security/exception/UsernameNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package nextstep.security.exception;
-
-public class UsernameNotFoundException extends RuntimeException {
-    public UsernameNotFoundException(String message) {
-        super(message);
-    }
-}
