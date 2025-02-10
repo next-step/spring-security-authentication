28b7224a236b559ca1439fedd11f38d9cef6fb90
yeongunheo <gjduddns@gmail.com>
2025-02-10 10:35:03 +0900

    feat: SecurityContextRepository 인터페이스를 기반으로 HttpSessionSecurityContextRepository 구현

--- src/main/java/nextstep/security/context/SecurityContextHolder.java
+++ src/main/java/nextstep/security/context/SecurityContextHolder.java
@@ -7,7 +7,7 @@ public class SecurityContextHolder {
     public static SecurityContext getContext() {
         SecurityContext context = contextHolder.get();
         if (context == null) {
-            context = new SecurityContextImpl();
+            context = createEmptyContext();
             contextHolder.set(context);
         }
         return context;
@@ -19,5 +19,9 @@ public class SecurityContextHolder {
 
     public static void clearContext() {
         contextHolder.remove();
+    }
+
+    public static SecurityContext createEmptyContext() {
+        return new SecurityContextImpl();
     }
 }
