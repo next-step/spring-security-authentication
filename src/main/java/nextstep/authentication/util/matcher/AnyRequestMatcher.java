33c00f76938b7d296e55b1fcb3b6b38904305323
yeongunheo <gjduddns@gmail.com>
2025-02-10 11:18:43 +0900

    refactor: AnyRequestMatcher 기본 생성자 private으로 변경

--- src/main/java/nextstep/authentication/util/matcher/AnyRequestMatcher.java
+++ src/main/java/nextstep/authentication/util/matcher/AnyRequestMatcher.java
@@ -6,6 +6,9 @@ public class AnyRequestMatcher implements RequestMatcher {
 
     public static final AnyRequestMatcher INSTANCE = new AnyRequestMatcher();
 
+    private AnyRequestMatcher() {
+    }
+
     @Override
     public boolean matches(HttpServletRequest request) {
         return true;
