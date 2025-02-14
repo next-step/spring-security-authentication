0bdb1e5e90f47ba00e70dbbe0f219bf4473971f4
yeongunheo <gjduddns@gmail.com>
2025-02-15 01:55:12 +0900

    refactor: ProviderManager.authenticate 내부를 stream을 사용하도록 수정

--- src/main/java/nextstep/authentication/ProviderManager.java
+++ src/main/java/nextstep/authentication/ProviderManager.java
@@ -4,6 +4,7 @@ import nextstep.authentication.exception.AuthenticationException;
 import nextstep.authentication.exception.ProviderNotFoundException;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ProviderManager implements AuthenticationManager {
 
@@ -17,30 +18,12 @@ public class ProviderManager implements AuthenticationManager {
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 
         Class<? extends Authentication> target = authentication.getClass();
-        Authentication result = null;
-        AuthenticationException lastException = null;
 
-        for (AuthenticationProvider provider : this.providers) {
-            if (provider.supports(target)) {
-
-                try {
-                    result = provider.authenticate(authentication);
-                    if (result != null) {
-                        break;
-                    }
-                } catch (AuthenticationException ex) {
-                    lastException = ex;
-                }
-            }
-        }
-
-        if (result != null) {
-            return result;
-        }
-        if (lastException == null) {
-            lastException = new ProviderNotFoundException(String.format("No AuthenticationProvider found for %s", target.getName()));
-        }
-
-        throw lastException;
+        return providers.stream()
+                .filter(provider -> provider.supports(target))
+                .map(provider -> provider.authenticate(authentication))
+                .filter(Objects::nonNull)
+                .findAny()
+                .orElseThrow(() -> new ProviderNotFoundException(String.format("No AuthenticationProvider found for %s", target.getName())));
     }
 }
